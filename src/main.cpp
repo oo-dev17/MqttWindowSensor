ad8cae5cfad1d5880c65b8520a87e22e58823f4d
oo-dev17 <costa.pavlis@gmail.com>
2026-08-21 18:37:51 +0200

    rename wifiConfig to personalConfig

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
-#include "wifiConfig.h"
+#include "personalConfig.h"
 #include <ESP8266HTTPClient.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>
